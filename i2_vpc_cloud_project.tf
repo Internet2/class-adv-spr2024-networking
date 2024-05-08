69559c1311b945436171ee316fa7abc6dde1caf9
ACE-336 <167769682+ACE-336@users.noreply.github.com>
2024-05-08 16:45:03 -0400

    Update i2_vpc_cloud_project.tf

--- i2_vpc_cloud_project.tf
+++ i2_vpc_cloud_project.tf
@@ -16,7 +16,6 @@ resource "google_compute_subnetwork" "i2_project_gcp_terraform_subnet_1" {
   ip_cidr_range = "10.2.0.0/24"
   network       = google_compute_network.i2_project_gcp_terraform_vpc.name
 }
-
 // ***AWS VPC Build***
 provider "aws" {
   // AWS credentials added as Windows environment variable in VScode settings.json file
@@ -37,4 +36,51 @@ resource "aws_subnet" "i2_project_aws_terraform_subnet_1" {
   tags = {
     Name = "i2_project_aws_terraform_subnet_1"
   }
+}
+
+// ***AWS Security Group***
+resource "aws_security_group" "allow_ssh_icmp_from_gcp" {
+  name        = "allow_ssh_icmp_from_gcp"
+  description = "Security group allowing SSH and ICMP from GCP"
+  vpc_id      = aws_vpc.i2_project_aws_terraform_vpc.id
+  
+  // ***AWS SSH and ICMP inbound rule from GCP***
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.2.0.0/24"]
+  }
+  
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["10.2.0.0/24"]
+  }
+  
+  // Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// ***GCP SSH and ICMP inbound rule from AWS***
+resource "google_compute_firewall" "allow_ssh_icmp_from_aws" {
+  name    = "allow-ssh-icmp-from-aws"
+  network = google_compute_network.i2_project_gcp_terraform_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+
+  source_ranges = ["172.16.1.0/24"] // Allow traffic from AWS CIDR
 }
